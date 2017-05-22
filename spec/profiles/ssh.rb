d37ea16b3827915bd2f96883ffacdea6cb03236d
Garrett Honeycutt <code@garretthoneycutt.com>
2017-05-22 18:13:01 -0400

    add example for security check

--- spec/profiles/ssh.rb
+++ spec/profiles/ssh.rb
@@ -3,5 +3,10 @@ shared_examples 'ssh' do
     describe port(22) do
       it { should be_listening }
     end
+
+    # Do not allow SSH as Root as per security policy #123
+#    describe file('/etc/ssh/sshd_config') do
+#      its(:content) { should match /^\s*PermitRootLogin\s*no\s*$/ }
+#    end
   end
 end
